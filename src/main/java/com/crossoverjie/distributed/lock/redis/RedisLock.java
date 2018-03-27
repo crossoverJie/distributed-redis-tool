ae02c4bf73540f2c1265ac819eb542c1ed8d09ce
crossoverJie <crossoverjie@gmail.com>
2018-03-27 23:47:23 +0800

    :tada: Initial commit.

---
+++ src/main/java/com/crossoverjie/distributed/lock/redis/RedisLock.java
@@ -0,0 +1,112 @@
+package com.crossoverjie.distributed.lock.redis;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.JedisCommands;
+
+import java.util.Collections;
+
+/**
+ * Function: 分布式锁
+ *
+ * @author crossoverJie
+ *         Date: 26/03/2018 11:09
+ * @since JDK 1.8
+ */
+public class RedisLock<T extends JedisCommands> {
+
+
+    private static final String LOCK_MSG = "OK";
+
+    private static final Long UNLOCK_MSG = 1L;
+    
+    private static final String SET_IF_NOT_EXIST = "NX";
+    private static final String SET_WITH_EXPIRE_TIME = "PX";
+
+    private static final String LOCK_PREFIX = "lock_" ;
+
+
+    private T jedis;
+
+    /**
+     * 时间 毫秒
+     */
+    private static final int TIME = 1000 ;
+
+    /**
+     * 加锁 非阻塞锁
+     * @param key
+     * @param request
+     *        请求
+     *
+     * 默认加锁 10 秒
+     * @return
+     */
+    public  boolean tryLock(String key, String request) {
+        String result = this.jedis.set(LOCK_PREFIX + key, request, SET_IF_NOT_EXIST, SET_WITH_EXPIRE_TIME, 10 * TIME);
+
+        if (LOCK_MSG.equals(result)){
+            return true ;
+        }else {
+            return false ;
+        }
+    }
+
+
+    /**
+     * 阻塞锁
+     * @param key
+     * @param request
+     */
+    public void lock(String key, String request){
+
+    }
+
+    /**
+     * 加锁 非阻塞锁
+     * @param key
+     * @param request
+     * @param expireTime
+     * @return
+     */
+    public  boolean tryLock( String key, String request, int expireTime) {
+        String result = this.jedis.set(LOCK_PREFIX + key, request, SET_IF_NOT_EXIST, SET_WITH_EXPIRE_TIME, expireTime);
+
+        if (LOCK_MSG.equals(result)){
+            return true ;
+        }else {
+            return false ;
+        }
+    }
+
+
+    /**
+     * 解锁
+     * @param key
+     * @param request
+     * @return
+     */
+    public  boolean unlock(String key,String request){
+        //lua 脚本
+        String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
+
+        Object result = null ;
+        if (jedis instanceof Jedis){
+            result = ((Jedis)this.jedis).eval(script, Collections.singletonList(LOCK_PREFIX + key), Collections.singletonList(request));
+        }else if (jedis instanceof JedisCluster){
+            result = ((JedisCluster)this.jedis).eval(script, Collections.singletonList(LOCK_PREFIX + key), Collections.singletonList(request));
+        }else {
+            throw new RuntimeException("instance is error") ;
+        }
+
+        if (UNLOCK_MSG.equals(result)){
+            return true ;
+        }else {
+            return false ;
+        }
+    }
+
+    public void setJedis(T jedis) {
+        this.jedis = jedis;
+    }
+}
---
+++ src/test/java/com/crossoverjie/distributed/lock/redis/RedisLockTest.java
@@ -0,0 +1,37 @@
+package com.crossoverjie.distributed.lock.redis;
+
+import org.junit.Before;
+import org.junit.Test;
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
+
+import java.util.UUID;
+
+import static org.junit.Assert.*;
+
+public class RedisLockTest {
+
+
+    private RedisLock redisLock;
+
+    @Before
+    public void setBefore() {
+        redisLock = new RedisLock();
+        HostAndPort hostAndPort = new HostAndPort("10.19.13.51", 7000);
+        JedisCluster jedisCluster = new JedisCluster(hostAndPort);
+        redisLock.setJedis(jedisCluster);
+    }
+
+    @Test
+    public void tryLock() throws Exception {
+        boolean locktest = redisLock.tryLock("reviewSubItem", UUID.randomUUID().toString());
+        System.out.println("locktest=" + locktest);
+    }
+
+    @Test
+    public void unlock() throws Exception {
+        boolean locktest = redisLock.unlock("test", "ec8ebca0-4ba0-4b23-99a8-b35fbba3629e");
+        System.out.println("locktest=" + locktest);
+    }
+
+}
