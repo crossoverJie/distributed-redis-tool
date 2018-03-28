2803161ac0cf81ce26228b97c14ec36cb7de1d1b
crossoverJie <crossoverjie@gmail.com>
2018-03-28 23:43:24 +0800

    :white_check_mark: Adding tests.

---
+++ src/test/java/com/crossoverjie/distributed/lock/redis/RedisTest.java
@@ -0,0 +1,46 @@
+package com.crossoverjie.distributed.lock.redis;
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
+
+/**
+ * Function:
+ *
+ * @author crossoverJie
+ *         Date: 28/03/2018 23:35
+ * @since JDK 1.8
+ */
+public class RedisTest {
+
+    @InjectMocks
+    private RedisLock redisLock;
+
+    @Mock
+    private Jedis jedis;
+
+    @Before
+    public void setBefore() {
+        MockitoAnnotations.initMocks(this);
+        redisLock.setJedis(jedis);
+
+    }
+
+    @Test
+    public void unlock() throws Exception {
+
+        Mockito.when(jedis.eval(Mockito.anyString(), Mockito.anyList(), Mockito.anyList())).thenReturn(1L) ;
+
+        boolean locktest = redisLock.unlock("test", "ec8ebca0-14ba0-4b23-99a8-b35fbba3629e");
+
+        Assert.assertTrue(locktest);
+
+        Mockito.verify(jedis).eval(Mockito.anyString(), Mockito.anyList(), Mockito.anyList());
+    }
+}
---
+++ src/test/java/com/crossoverjie/distributed/lock/redis/WrongInstanceTest.java
@@ -0,0 +1,44 @@
+package com.crossoverjie.distributed.lock.redis;
+
+import junit.framework.Assert;
+import org.easymock.internal.RuntimeExceptionWrapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.ShardedJedis;
+
+/**
+ * Function:
+ *
+ * @author crossoverJie
+ *         Date: 28/03/2018 23:35
+ * @since JDK 1.8
+ */
+public class WrongInstanceTest {
+
+    @InjectMocks
+    private RedisLock redisLock;
+
+    @Mock
+    private ShardedJedis jedis;
+
+    @Before
+    public void setBefore() {
+        MockitoAnnotations.initMocks(this);
+        redisLock.setJedis(jedis);
+
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void unlock() throws Exception {
+
+        Mockito.when(redisLock.unlock(Mockito.anyString(),Mockito.anyString())).thenThrow(RuntimeException.class) ;
+        redisLock.unlock("test", "ec8ebca0-14ba0-4b23-99a8-b35fbba3629e");
+
+
+    }
+}
