bf36a16997a9ed22cb4b8dc4ddc401f70589af3b
crossoverJie <crossoverjie@gmail.com>
2018-03-28 22:56:31 +0800

    :white_check_mark: Adding tests.

--- src/test/java/com/crossoverjie/distributed/lock/redis/RedisLockTest.java
+++ src/test/java/com/crossoverjie/distributed/lock/redis/RedisLockTest.java
@@ -1,41 +1,62 @@
 package com.crossoverjie.distributed.lock.redis;
 
+import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.JedisCluster;
 
 import java.util.UUID;
 
-import static org.junit.Assert.*;
 
 public class RedisLockTest {
 
 
+    @InjectMocks
     private RedisLock redisLock;
+
+    @Mock
+    private JedisCluster jedisCluster;
 
     @Before
     public void setBefore() {
-        redisLock = new RedisLock();
-        HostAndPort hostAndPort = new HostAndPort("10.19.13.51", 7000);
-        JedisCluster jedisCluster = new JedisCluster(hostAndPort);
+        MockitoAnnotations.initMocks(this);
         redisLock.setJedis(jedisCluster);
+
+        //redisLock = new RedisLock();
+        //HostAndPort hostAndPort = new HostAndPort("10.19.13.51", 7000);
+        //JedisCluster jedisCluster = new JedisCluster(hostAndPort);
     }
 
     @Test
     public void tryLock() throws Exception {
-        boolean locktest = redisLock.tryLock("test", UUID.randomUUID().toString());
+        String key = "test";
+        String request = UUID.randomUUID().toString();
+        Mockito.when(jedisCluster.set(Mockito.anyString(), Mockito.anyString(), Mockito.anyString(),
+                Mockito.anyString(), Mockito.anyLong())).thenReturn("OK");
+
+        boolean locktest = redisLock.tryLock(key, request);
         System.out.println("locktest=" + locktest);
+
+        Assert.assertTrue(locktest);
     }
 
     @Test
     public void tryLock2() throws Exception {
-        boolean locktest = redisLock.tryLock("test", UUID.randomUUID().toString(),10 * 1000);
+        boolean locktest = redisLock.tryLock("test", UUID.randomUUID().toString(), 10 * 1000);
         System.out.println("locktest=" + locktest);
     }
 
     @Test
     public void lock() throws Exception {
+
+        Mockito.when(jedisCluster.set(Mockito.anyString(), Mockito.anyString(), Mockito.anyString(),
+                Mockito.anyString(), Mockito.anyLong())).thenReturn("OK");
+
         long start = System.currentTimeMillis();
         redisLock.lock("test", UUID.randomUUID().toString());
         long end = System.currentTimeMillis();
@@ -48,7 +69,7 @@ public class RedisLockTest {
         long start = System.currentTimeMillis();
         boolean lock = redisLock.lock("test", UUID.randomUUID().toString(), 100);
         long end = System.currentTimeMillis();
-        System.out.println("lock success expire=" + (end - start) + " lock = " +lock) ;
+        System.out.println("lock success expire=" + (end - start) + " lock = " + lock);
     }
 
     @Test
