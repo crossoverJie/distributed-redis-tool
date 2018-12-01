51dfaeba9978388283e2b9d7c1cadaa48317b8f7
johnxue2013 <johnxue2013@foxmail.com>
2018-12-01 17:44:28 +0800

    Update ScriptUtil.java
    release stream after use

--- src/main/java/com/crossoverjie/distributed/util/ScriptUtil.java
+++ src/main/java/com/crossoverjie/distributed/util/ScriptUtil.java
@@ -19,10 +19,8 @@ public class ScriptUtil {
      */
     public static String getScript(String path) {
         StringBuilder sb = new StringBuilder();
-
-        InputStream stream = ScriptUtil.class.getClassLoader().getResourceAsStream(path);
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(stream))){
-
+        
+        try (InputStream stream = ScriptUtil.class.getClassLoader().getResourceAsStream(path);BufferedReader br = new BufferedReader(new InputStreamReader(stream))){
             String str;
             while ((str = br.readLine()) != null) {
                 sb.append(str).append(System.lineSeparator());
